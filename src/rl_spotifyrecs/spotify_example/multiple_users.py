b29e6fc9fc3850aebc83d89a345a91908ee1a235
Asr419 <asrthebest17@gmail.com>
2023-11-08 20:14:08 +0000

    spotify_data

--- src/rl_spotifyrecs/spotify_example/multiple_users.py
+++ src/rl_spotifyrecs/spotify_example/multiple_users.py
@@ -25,39 +25,53 @@ Users = [element.strip() for element in Users if element.strip()]
 
 # Create a list to store the Spotify API objects for each user
 sp_users = []
+playlist_name = 'Playlist by Aayush'
+playlist_description = 'A dynamic playlist'
 
 
 
 # Authenticate and create Spotify API objects for each user
+# for user in Users:
+#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='user-read-playback-state', username=user))
+#     sp_users.append(sp)
+
 for user in Users:
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='user-read-playback-state', username=user))
-    sp_users.append(sp)
+    
 
-for i, sp in enumerate(sp_users):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='user-library-read user-read-playback-state user-read-currently-playing user-modify-playback-state playlist-modify-public playlist-modify-private', username=user))
+    #get user playlist
+    playlists = sp.user_playlists(user)
+    #get user current track
     current_track = sp.current_playback()
+    target_playlist = None
+    for playlist in playlists['items']:
+        if playlist['name'] == playlist_name:
+            target_playlist = playlist
+            break
+    #delete playlist
     if current_track is not None:
-        print(f"User {Users[i]} is listening to {current_track['item']['name']} by {current_track['item']['artists'][0]['name']}")
-    else:
-        print(f"User {Users[i]} is not currently listening to anything.")
+        if target_playlist:
+            sp.user_playlist_unfollow(user, target_playlist['id'])
+     
+        seed_tracks = [current_track['item']['uri']]
+        print(f"User {user} is listening to {current_track['item']['name']} by {current_track['item']['artists'][0]['name']}")
 
-# # Get the current playing track for each user using Spotify Web API
-# for i, sp in enumerate(sp_users):
-#     user_id = Users[i]
-#     access_token = sp.auth_manager.get_access_token(as_dict=False)
-#     headers = {"Authorization": f"Bearer {access_token}"}
-    
-#     response = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers=headers)
-    
-#     if response.status_code == 200:
-#         current_track = response.json()
-#         if current_track.get('item'):
-#             print(f"User {user_id} is listening to {current_track['item']['name']} by {current_track['item']['artists'][0]['name']}")
-#         else:
-#             print(f"User {user_id} is not currently listening to anything sorry.")
-#     elif response.status_code == 204:
-#         print(f"User {user_id} is not currently listening to anything sorry.")
-#     else:
-#         print(f"Error for User {user_id}: {response.status_code} - {response.text}")
+        #get recommendations
+        recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=10)
+        all_tracks = recommendations['tracks']
+        # print(all_tracks)
+        # all_tracks = [item for item in all_tracks if 'track' in item]
+        # Shuffle the combined tracks
+        random.shuffle(all_tracks)
+
+        playlist = sp.user_playlist_create(sp.me()['id'], name=playlist_name, public=True, description=playlist_description)
+        # print(all_tracks)
+        track_uris = [track['uri'] for track in all_tracks]
+        
+        sp.playlist_add_items(playlist['id'], track_uris)
+    else:
+        print(f"User {user} is not currently listening to anything.")
+
 
 
 
